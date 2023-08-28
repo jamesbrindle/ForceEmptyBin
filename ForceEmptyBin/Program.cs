7055c0ece40b76dfa6a854f1b1c277b363d2f4d6
Jamie Brindle <jamie.brindle7@gmail.com>
2023-08-28 16:27:29 +0100

    Init

---
+++ ForceEmptyBin/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace ForceEmptyBin
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            if (!IsRunAsAdmin())
+            {
+                ProcessStartInfo proc = new ProcessStartInfo
+                {
+                    UseShellExecute = true,
+                    WorkingDirectory = Environment.CurrentDirectory,
+                    FileName = Assembly.GetEntryAssembly().CodeBase,
+                    Verb = "runas"
+                };
+
+                try
+                {
+                    Process.Start(proc);
+
+                    for (char c = 'A'; c < 'Z'; c++)
+                        RunForceEmptyBin(c);
+
+                    UseNirCmd();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("This program must be run as an administrator! \n\n" + ex.ToString());
+                }
+            }
+        }
+
+        private static void RunForceEmptyBin(char driveLetter)
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/C rd /s /q {driveLetter}:\\$Recycle.bin";
+                process.Start();
+                process.WaitForExit();
+            }
+            catch { }
+        }
+
+        private static void UseNirCmd()
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.Verb = "runas";
+
+                process.StartInfo.FileName = "nircmd.exe";
+                process.StartInfo.Arguments = "emptybin";
+                process.Start();
+                process.WaitForExit();
+            }
+            catch { }
+        }
+
+        private static bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ForceEmptyBin
+
 A simple app for emptying the recycling, specifically for 'sticky' deleted objects.
+
+You tend tend to get 'sticky' deleted objects (those that don't delete when emptying the bin) on SharePoint syncrhonised folders in OneDrive. When a user deletes a file or folder in SharePoint, and you sync your folder to the same SharePoint location, that file or folder ends up in the recyling bin but it seems you can't delete it.
+
+The solution is to simply open a command prompt as adminstrator and run: "rd /s /q {driveLetter}:\\$Recycle.bin"
+
+This app does just this, by iterating over your drives and running this delete command.
+
+It also uses NirCmd.exe to empty the bin, just to finish it off.
